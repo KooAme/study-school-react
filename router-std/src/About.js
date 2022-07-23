7bfa489de2d959cd41fc569f8bec3fdec74910a9
Koobori <dg.changeworld@gmail.com>
2022-07-23 21:36:05 +0900

    React Router
    std

---
+++ router-std/src/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <>
+      <div>
+        <h1>紹介！</h1>
+        <p>このプロジェクトを紹介します。</p>
+      </div>
+    </>
+  );
+};
+
+export default About;
---
+++ router-std/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
+import About from './About';
+import Home from './Home';
+
+function App() {
+  return (
+    <>
+      <Route path='/' component={Home} />
+      <Route path='/about' component={About} />
+    </>
+  );
+}
+
+export default App;
